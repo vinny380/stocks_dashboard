a3f7474618d76c7fdc160c42f8a37f99df0048fa
Your Name <vinny.purgato@gmail.com>
2023-02-21 21:55:39 -0500

    percentage of daily lost/gained value

--- run.py
+++ run.py
@@ -14,6 +14,18 @@ st.sidebar.title('Options')
 
 df_tesla.Date = df_tesla.Date.apply(lambda x: datetime.strptime(x, '%Y-%m-%d'))
 df_intel.Date = df_intel.Date.apply(lambda x: datetime.strptime(x, '%Y-%m-%d'))
+col1, col2 = st.columns(2)
+
+
+def red_or_green_daily(dfs):
+    for n in dfs:
+        if (float(n.Close.iloc[-1]) > (float(n.Close.iloc[-2]))):
+            st.write(f"Tesla is up by ${float(n.Close.iloc[-1]) - (float(n.Close.iloc[-2]))}")
+        else:
+            st.write(f'''Tesla is down by ${float(n.Close.iloc[-2]) - (float(n.Close.iloc[-1]))}
+                    \n-{100 - (float(n.Close.iloc[-1])/float(n.Close.iloc[-2])) * 100}%''')
+
+red_or_green_daily([df_tesla])
 
 
 
@@ -38,3 +50,20 @@ def hist_plot(categories, stocks):
 hist = st.sidebar.button('Histogram')
 if hist:
     hist_plot(categories, stocks)
+
+
+def line_plot(categories, stocks):
+    fig, ax = plt.subplots(figsize=(20,6))
+    for categorie in categories:
+        for stock in stocks:
+            if stock == 'Tesla':
+                df = df_tesla
+            elif stock == 'Intel':
+                df = df_intel
+            ax.plot('Date',categorie,data=df)
+            ax.set_title(f"{str(categorie)} - {stock}")
+            ax.legend()
+    st.pyplot(fig)
+line = st.sidebar.button('Line Plot')
+if line:
+    line_plot(categories, stocks)
